94e4345c65d267048cfde3fdaeed0ecb65ec12f0
Krzysztof Wojdalski <krzysztof.wojdalski@gmail.com>
2026-04-24 17:36:13 +0200

    Export StockDataFetcher

--- src/trading_rl/data_fetchers/__init__.py
+++ src/trading_rl/data_fetchers/__init__.py
@@ -6,10 +6,12 @@ from trading_rl.data_fetchers.base import (
     BaseSyntheticGenerator,
 )
 from trading_rl.data_fetchers.download_tracker import DownloadTracker
+from trading_rl.data_fetchers.stock_fetcher import StockDataFetcher
 
 __all__ = [
     "BaseDataSource",
     "BaseMarketDataFetcher",
     "BaseSyntheticGenerator",
     "DownloadTracker",
+    "StockDataFetcher",
 ]
